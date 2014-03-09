ba40a0dd1393cbd0f1faf0cb44069a5527944d05
Jingyuan <jingyuan4ever@gmail.com>
2014-03-09 16:28:25 +0800

    solve hamming distance 2

---
+++ hamming-distance2.py
@@ -0,0 +1,12 @@
+__author__ = 'jingyuan'
+
+
+def checkio(n, m):
+    return str(bin(n ^ m)).count('1')
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
