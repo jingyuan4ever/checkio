5c731da94ad9ff5af2ffdf53f452433011d79f59
Jingyuan <jingyuan4ever@gmail.com>
2014-03-14 23:06:28 +0800

    solve skew-symmetric-matrix

---
+++ skew-symmetric-matrix.py
@@ -0,0 +1,25 @@
+__author__ = 'jingyuan'
+
+def checkio(matrix):
+    length = len(matrix)
+    for col in range(length):
+        for row in range(col+1):
+            if matrix[col][row] != -matrix[row][col]:
+                return False
+    return True
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]) == True, "1st example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 1, 1],
+        [-2, -1, 0]]) == False, "2nd example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-3, -1, 0]]) == False, "3rd example"
