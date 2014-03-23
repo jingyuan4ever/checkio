377a4d43d49b80898be129739ac78e573b430c0f
Jingyuan <jingyuan4ever@gmail.com>
2014-03-23 23:51:10 +0800

    solve even-last

---
+++ even-last.py
@@ -0,0 +1,15 @@
+__author__ = 'jingyuan'
+def checkio(array):
+    """
+        sums even-indexes elements and multiply at the last
+    """
+    if len(array) == 0:
+        return 0
+    return sum(array[::2])*array[-1]
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "Empty"
