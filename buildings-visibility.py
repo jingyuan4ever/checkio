c9b6312c36f9c33698f588bc5af096fe0440d196
Jingyuan <jingyuan4ever@gmail.com>
2014-03-24 20:13:15 +0800

    solve buildings-visibility

---
+++ buildings-visibility.py
@@ -0,0 +1,54 @@
+__author__ = 'jingyuan'
+
+
+def checkio(buildings):
+    buildings.sort(key=lambda x: x[1])
+    count = 0
+    vision = dict()
+    for building in buildings:
+        visible = False
+        s, e, h = building[0], building[2], building[4]
+        for x in range(s, e):
+            if x in vision.keys():
+                if h > vision[x]:
+                    vision[x] = h
+                    visible = True
+            else:
+                vision[x] = h
+                visible = True
+        if visible:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    assert checkio([
+        [1, 1, 4, 5, 3.5],
+        [2, 6, 4, 8, 5],
+        [5, 1, 9, 3, 6],
+        [5, 5, 6, 6, 8],
+        [7, 4, 10, 6, 4],
+        [5, 7, 10, 8, 3]
+    ]) == 5, "First"
+    assert checkio([
+        [1, 1, 11, 2, 2],
+        [2, 3, 10, 4, 1],
+        [3, 5, 9, 6, 3],
+        [4, 7, 8, 8, 2]
+    ]) == 2, "Second"
+    assert checkio([
+        [1, 1, 3, 3, 6],
+        [5, 1, 7, 3, 6],
+        [9, 1, 11, 3, 6],
+        [1, 4, 3, 6, 6],
+        [5, 4, 7, 6, 6],
+        [9, 4, 11, 6, 6],
+        [1, 7, 11, 8, 3.25]
+    ]) == 4, "Third"
+    assert checkio([
+        [0, 0, 1, 1, 10]
+    ]) == 1, "Alone"
+    assert checkio([
+        [2, 2, 3, 3, 4],
+        [2, 5, 3, 6, 4]
+    ]) == 1, "Shadow"
