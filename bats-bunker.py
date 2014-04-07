2cc1de22487fe07f764c76b62b8634a799856e0a
Jingyuan <jingyuan4ever@gmail.com>
2014-04-07 10:53:01 +0800

    solve bats-bunker

--- bats-bunker.py
+++ bats-bunker.py
@@ -23,11 +23,36 @@ def canPass(x1, x2, y1, y2, bunker):
                 return False
         return True
 
-    k = Fraction((x1 - x2), (y1 - y2))
-    for i in range(x1, x2 + 1):
-        for j in range(y1, y2 + 1):
-            if bunker[i][j] == 'W':
+    if abs(dx) >= abs(dy):
+        k = Fraction(dy, dx)
+        if dx > 0:
+            x1, y1, x2, y2 = x2, y2, x1, y1
+        start = y1 + k/2
+        for i in range(abs(dx)):
+            nowy = start + i * k
+            nowx = x1 + i
+            if bunker[nowx][int(nowy+.5)] == 'W' or bunker[nowx+1][int(nowy+.5)] == 'W':
                 return False
+            if int(nowy+.5) == nowy+.5:
+                if bunker[nowx][int(nowy+.5)-1] == 'W' or bunker[nowx+1][int(nowy+.5)-1] == 'W':
+                    return False
+        return True
+
+    if abs(dx) < abs(dy):
+        k = Fraction(dx, dy)
+        if dy > 0:
+            x1, y1, x2, y2 = x2, y2, x1, y1
+        start = x1 + k/2
+        for i in range(abs(dy)):
+            nowx = start + i * k
+            nowy = y1 + i
+            if bunker[int(nowx+.5)][nowy] == 'W' or bunker[int(nowx+.5)][nowy+1] == 'W':
+                return False
+            if int(nowx+.5) == nowx+.5:
+                if bunker[int(nowx+.5)-1][nowy] == 'W' or bunker[int(nowx+.5)-1][nowy+1] == 'W':
+                    return False
+        return True
+
     return True
 
 
@@ -61,7 +86,7 @@ def checkio(bunker):
     active = (0, 0)
     open = [active]
     closed = []
-    status = {}
+    status = dict()
     status[active] = (0, distance(active, dest), None)
     while active != dest:
         open.remove(active)
@@ -72,7 +97,7 @@ def checkio(bunker):
                 continue
             if n not in open:
                 open.append(n)
-                status[n] = (status[active][0] + distance(n, active), distance(n, dest), active)
+                status[n] = [status[active][0] + distance(n, active), distance(n, dest), active]
             else:
                 disg = status[active][0] + distance(n, active)
                 if status[n][0] > disg:
@@ -85,7 +110,6 @@ def checkio(bunker):
                 active = i
     now = active
     while status[now][2]:
-        print now
         now = status[now][2]
 
     return status[active][0] + status[active][1]
@@ -96,25 +120,27 @@ if __name__ == '__main__':
         precision = 0.1 ** significant_digits
         return correct - precision < checked < correct + precision
 
-    # assert almost_equal(checkio([
-    #     "B--",
-    #     "---",
-    #     "--A"]), 2.83), "1st example"
-    # assert almost_equal(checkio([
-    #     "B-B",
-    #     "BW-",
-    #     "-BA"]), 4), "2nd example"
-    # assert almost_equal(checkio([
-    #     "BWB--B",
-    #     "-W-WW-",
-    #     "B-BWAB"]), 12), "3rd example"
-    # assert almost_equal(checkio([
-    #     "B---B-",
-    #     "-WWW-B",
-    #     "-WA--B",
-    #     "-W-B--",
-    #     "-WWW-B",
-    #     "B-BWB-"]), 9.24), "4th example"
-    #
-    # assert almost_equal(checkio(["B-B-B", "-WWW-", "BWA-B", "-WWW-", "B-B-B"]), 8), "5th example"
+    assert almost_equal(checkio([
+        "B--",
+        "---",
+        "--A"]), 2.83), "1st example"
+    assert almost_equal(checkio([
+        "B-B",
+        "BW-",
+        "-BA"]), 4), "2nd example"
+    assert almost_equal(checkio([
+        "BWB--B",
+        "-W-WW-",
+        "B-BWAB"]), 12), "3rd example"
+    assert almost_equal(checkio([
+        "B---B-",
+        "-WWW-B",
+        "-WA--B",
+        "-W-B--",
+        "-WWW-B",
+        "B-BWB-"]), 9.24), "4th example"
+
+    assert almost_equal(checkio(["B-B-B", "-WWW-", "BWA-B", "-WWW-", "B-B-B"]), 8), "5th example"
     assert almost_equal(checkio(["BWA-B-", "-W----", "-WW-B-", "-W---B", "--B---", "B-B---"]), 12.83), "6th example"
+    checkio(["BBBBB","BBBBB","BBBBB","BBBBB","BBBBA"])
+
