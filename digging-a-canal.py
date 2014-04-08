a8d0aeacaa0a45ed77f8f1ee03b2b167e888c8c9
Jingyuan <jingyuan4ever@gmail.com>
2014-04-08 16:32:33 +0800

    solve digging-a-canal

--- digging-a-canal.py
+++ digging-a-canal.py
@@ -1,9 +1,38 @@
 __author__ = 'jingyuan'
+from operator import add
 
 def checkio(data):
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    width = len(data)
+    height = len(data[0])
 
+    k = 0
+    opn = [(0, j) for j in range(height) if data[0][j] == 0]
+    dig = [(0, j) for j in range(height) if data[0][j] == 1]
+    cls = []
+    while True:
+        while opn:
+            now = opn.pop(0)
+            cls.append(now)
+            if now[0] == width - 1:
+                return k
+            for d in dirs:
+                nxt = tuple(map(add, now, d))
+                if nxt[0] in range(0, width) and nxt[1] in range(0, height):
+                    if nxt in cls:
+                        continue
+                    if nxt in opn:
+                        continue
+                    if data[nxt[0]][nxt[1]] == 0:
+                        opn.append(nxt)
+                    else:
+                        dig.append(nxt)
+        k += 1
+        opn = dig
+        dig = []
 
     return 0
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -29,3 +58,6 @@ if __name__ == '__main__':
                     [1, 0, 1, 1, 1, 1, 1, 1],
                     [1, 0, 1, 1, 1, 1, 1, 1],
                     [1, 1, 1, 0, 1, 1, 1, 1]]) == 2, "3rd example"
+    assert checkio(
+        [[0, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0],
+         [1, 1, 0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 0, 1, 1, 0], [0, 1, 1, 1, 0, 0, 0, 0]]) == 0
