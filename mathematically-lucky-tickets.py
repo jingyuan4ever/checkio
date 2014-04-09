600ef89caba54ce21911db7d5a8dac6a7f443528
Jingyuan <jingyuan4ever@gmail.com>
2014-04-09 19:55:34 +0800

    solve mathematically-lucky-tickets

--- mathematically-lucky-tickets.py
+++ mathematically-lucky-tickets.py
@@ -1,18 +1,40 @@
 __author__ = 'jingyuan'
-import itertools
+import operator
 from fractions import Fraction
 
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div
+}.items()
 
-def checkio(data):
-    ops = itertools.product("01234", repeat=5)
-    seqs = itertools.permutations("01234")
 
-    # for op in ops:
-    #     for seq in seqs:
+def checkio(ticket):
+    subresults = {}
 
+    def calculate(start, end):
+        p = (start, end)
+        if p not in subresults:
+            n = ticket[start:end + 1]
+            sr = subresults[p] = {int(n): n}
+            for s in xrange(start, end):
+                for op_name, op_func in OPERATORS:
+                    for r1, e1 in calculate(start, s).items():
+                        for r2, e2 in calculate(s + 1, end).items():
+                            try:
+                                res = op_func(Fraction(r1), Fraction(r2))
+                            except ZeroDivisionError:
+                                continue
+                            if res not in sr:
+                                sr[res] = '(%s %s %s)' % (e1, op_name, e2)
+        return subresults[p]
 
-    #replace this for solution
-    return True or False
+    results = calculate(0, len(ticket) - 1)
+    if 100 in results:
+        return False
+    else:
+        return True
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -20,3 +42,5 @@ if __name__ == '__main__':
     assert checkio(u'707409') == True, "You can not transform it to 100"
     assert checkio(u'595347') == False, "(5 + ((9 / (3 / 34)) - 7)) = 100"
     assert checkio(u'271353') == False, "(2 - (7 * (((1 / 3) - 5) * 3))) = 100"
+    assert checkio(u"100478") == True
+    assert checkio(u"836403") == False
