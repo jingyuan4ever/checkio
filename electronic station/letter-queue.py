a646322f6670b44714102846bb7b7b194fca1f89
jingyuan4ever <jingyuan4ever@gmail.com>
2014-12-30 00:23:12 +0800

    add weak point

--- electronic station/letter-queue.py
+++ electronic station/letter-queue.py
@@ -8,7 +8,7 @@ def letter_queue(commands):
                 l.pop(0)
     return ''.join(l)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert letter_queue(["PUSH A", "POP", "POP", "PUSH Z", "PUSH D", "PUSH O", "POP", "PUSH T"]) == "DOT", "dot example"
     assert letter_queue(["POP", "POP"]) == "", "Pop, Pop, empty"
---
+++ electronic station/weak-point.py
@@ -0,0 +1,32 @@
+def weak_point(matrix):
+    m = 10000
+    minI = 0
+    minJ = 0
+    for i, line in enumerate(matrix):
+        if m > sum(line):
+            m = sum(line)
+            minI = i
+    m = 10000
+    for j, line in enumerate(zip(*matrix)):
+        if m > sum(line):
+            m = sum(line)
+            minJ = j
+    return minI, minJ
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
+    assert list(weak_point([[7, 2, 7, 2, 8],
+                            [2, 9, 4, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [3, 3], "Example"
+    assert list(weak_point([[7, 2, 4, 2, 8],
+                            [2, 8, 1, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [1, 2], "Two weak point"
+    assert list(weak_point([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]])) == [0, 0], "Top left"
