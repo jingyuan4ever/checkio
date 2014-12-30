bba1f333c40b1df29bb4f7b71054dd3bd9cd750a
jingyuan4ever <jingyuan4ever@gmail.com>
2014-12-30 18:28:48 +0800

    add moore neighbor

---
+++ electronic station/moore_neighbourhood.py
@@ -0,0 +1,27 @@
+__author__ = 'jingyuan'
+
+def count_neighbours(grid, row, col):
+
+    g = lambda x, w: filter(lambda z: 0 <= z < w, map(lambda y: x+y, range(-1, 2)))
+    return sum([grid[x][y] for x in g(row, len(grid)) for y in g(col, len(grid[0])) if x != row or y != col])
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(
+        ((1, 0, 0, 1, 0),
+         (0, 1, 0, 0, 0),
+         (0, 0, 1, 0, 1),
+         (1, 0, 0, 0, 0),
+         (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
